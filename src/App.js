ab2cf4a4880d8e525016c03753c626abaa404f46
suminHeo <>
2023-09-17 21:28:16 +0900

    fix: 최신 버전에 맞춰 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Detail from "./routes/Detail";
@@ -9,9 +9,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/movie/:id" component={Detail} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </Router>
   );
