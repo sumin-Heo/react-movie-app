cce02aff6862252722a72bc11b52977f639af0d0
suminHeo <>
2023-09-17 21:09:18 +0900

    Initial app

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About(props) {
+  console.log(props);
+  return (
+    <div className="about__container">
+      <span>"About"</span>
+    </div>
+  );
+}
+
+export default About;
