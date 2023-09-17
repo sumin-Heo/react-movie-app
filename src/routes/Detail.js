af8e7f867149b859296541515b1ac982122ba801
suminHeo <>
2023-09-17 22:11:03 +0900

    fix: movie state 가 없다면 home 으로 back

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,25 +1,38 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Detail() {
   const location = useLocation();
+  const navigate = useNavigate();
   const movie = location.state;
+
+  useEffect(() => {
+    if (!movie) {
+      navigate("/");
+    }
+  }, [movie, navigate]);
 
   if (!movie) {
     return null;
   }
-
   return (
     <div className="detail">
-      <h1>{movie.title}</h1>
-      <img src={movie.poster} alt={movie.title} />
-      <h2>{movie.year}</h2>
-      <ul>
-        {movie.genres.map((genre, index) => (
-          <li key={index}>{genre}</li>
-        ))}
-      </ul>
-      <p>{movie.summary}</p>
+      <img src={movie.poster} alt={movie.title} style={{ width: "300px" }} />
+      <div style={{ marginLeft: "20px" }}>
+        <h1 style={{ marginBottom: "20px" }}>{movie.title}</h1>
+        <h2 style={{ marginBottom: "20px" }}>{movie.year}</h2>
+        <ul style={{ listStyle: "none", padding: "0", marginBottom: "20px" }}>
+          {movie.genres.map((genre, index) => (
+            <li
+              key={index}
+              style={{ display: "inline-block", marginRight: "10px" }}
+            >
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p style={{ whiteSpace: "pre-wrap" }}>{movie.summary}</p>
+      </div>
     </div>
   );
 }
